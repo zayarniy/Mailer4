4dff977cd272de3b914264eb790b22555ff1c887
Андрей Заярный <zaazaa@yandex.ru>
2021-07-08 19:36:29 +0300

    Lesson2 - begin

--- Mailer/Model.cs
+++ Mailer/Model.cs
@@ -7,12 +7,10 @@ namespace Mailer
 {
     public class Model:INotifyPropertyChanged
     {
-<<<<<<< HEAD
       
         //Add something
-=======
         static int Counter { get; set; } = 0;
->>>>>>> b6d8cf8acfdc4e24dfa78ed21d0e602e7afe7d3f
+
         public string Login { get; set; }
         public string Password { get; set; }
 
