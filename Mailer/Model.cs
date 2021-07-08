8e57afb65651f708a7de123615061098fab1a6d5
Андрей Заярный <zaazaa@yandex.ru>
2021-07-08 19:45:23 +0300

    Lessob2

--- Mailer/Model.cs
+++ Mailer/Model.cs
@@ -7,12 +7,11 @@ namespace Mailer
 {
     public class Model:INotifyPropertyChanged
     {
-<<<<<<< HEAD
       
         //Add something
-=======
+        //Add something
         static int Counter { get; set; } = 0;
->>>>>>> b6d8cf8acfdc4e24dfa78ed21d0e602e7afe7d3f
+
         public string Login { get; set; }
         public string Password { get; set; }
 
